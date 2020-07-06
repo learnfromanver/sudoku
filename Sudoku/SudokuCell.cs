896f63291661a0cbc3a859ded7ada5c8bc3e550f
learnfromanver <63973978+learnfromanver@users.noreply.github.com>
2020-07-06 13:33:04 +0530

    Added missing cs files

---
+++ Sudoku/SudokuCell.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace sudoku
+{
+    class SudokuCell : Button
+    {
+        public int Value { get; set; }
+        public bool IsLocked { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Clear()
+        {
+            this.Text = string.Empty;
+            this.IsLocked = false;
+        }
+    }
+}
