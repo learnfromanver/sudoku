aa6255266d2fa3da6ae02d38cb949559941cd729
learnfromanver <63973978+learnfromanver@users.noreply.github.com>
2020-07-06 13:30:13 +0530

    First Commit

---
+++ Sudoku/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.beginnerLevel = new System.Windows.Forms.RadioButton();
+            this.IntermediateLevel = new System.Windows.Forms.RadioButton();
+            this.AdvancedLevel = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(76, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(630, 630);
+            this.panel1.TabIndex = 0;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGameButton.Location = new System.Drawing.Point(712, 520);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(179, 103);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // checkButton
+            // 
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(712, 76);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(179, 100);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Check Input";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(712, 193);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(179, 59);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear Input";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // beginnerLevel
+            // 
+            this.beginnerLevel.AutoSize = true;
+            this.beginnerLevel.Checked = true;
+            this.beginnerLevel.Location = new System.Drawing.Point(712, 430);
+            this.beginnerLevel.Name = "beginnerLevel";
+            this.beginnerLevel.Size = new System.Drawing.Size(98, 24);
+            this.beginnerLevel.TabIndex = 3;
+            this.beginnerLevel.TabStop = true;
+            this.beginnerLevel.Text = "Beginner";
+            this.beginnerLevel.UseVisualStyleBackColor = true;
+            // 
+            // IntermediateLevel
+            // 
+            this.IntermediateLevel.AutoSize = true;
+            this.IntermediateLevel.Location = new System.Drawing.Point(712, 460);
+            this.IntermediateLevel.Name = "IntermediateLevel";
+            this.IntermediateLevel.Size = new System.Drawing.Size(124, 24);
+            this.IntermediateLevel.TabIndex = 3;
+            this.IntermediateLevel.Text = "Intermediate";
+            this.IntermediateLevel.UseVisualStyleBackColor = true;
+            // 
+            // AdvancedLevel
+            // 
+            this.AdvancedLevel.AutoSize = true;
+            this.AdvancedLevel.Location = new System.Drawing.Point(712, 490);
+            this.AdvancedLevel.Name = "AdvancedLevel";
+            this.AdvancedLevel.Size = new System.Drawing.Size(105, 24);
+            this.AdvancedLevel.TabIndex = 3;
+            this.AdvancedLevel.Text = "Advanced";
+            this.AdvancedLevel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(706, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Level";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 795);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AdvancedLevel);
+            this.Controls.Add(this.IntermediateLevel);
+            this.Controls.Add(this.beginnerLevel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Sudoku";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.RadioButton beginnerLevel;
+        private System.Windows.Forms.RadioButton IntermediateLevel;
+        private System.Windows.Forms.RadioButton AdvancedLevel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sudoku/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            createCells();
+
+            startNewGame();
+        }
+
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Create 81 cells for with styles and locations based on the index
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Black;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    // Assign key press event for each cells
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+
+            // Do nothing if the cell is locked
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            // Add the pressed key value in the cell only if it is a number
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                // Clear the cell value if pressed key is zero
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+        
+        private void startNewGame()
+        {
+            loadValues();
+
+            var hintsCount = 0;
+
+            // Assign the hints count based on the 
+            // level player chosen
+            if (beginnerLevel.Checked)
+                hintsCount = 45;
+            else if (IntermediateLevel.Checked)
+                hintsCount = 30;
+            else if (AdvancedLevel.Checked)
+                hintsCount = 15;
+
+            showRandomValuesHints(hintsCount);
+        }
+
+        private void showRandomValuesHints(int hintsCount)
+        {
+            // Show value in radom cells
+            // The hints count is based on the level player choose
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                // Style the hint cells differently and
+                // lock the cell so that player can't edit the value
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+
+        private void loadValues()
+        {
+            // Clear the values in each cells
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+
+            // This method will be called recursively 
+            // until it finds suitable values for each cells
+            findValueForNextCell(0, -1);
+        }
+
+        Random random = new Random();
+
+        private bool findValueForNextCell(int i, int j)
+        {
+            // Increment the i and j values to move to the next cell
+            // and if the columsn ends move to the next row
+            if (++j > 8)
+            {
+                j = 0;
+
+                // Exit if the line ends
+                if (++i > 8)
+                    return true;
+            }
+
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Find a random and valid number for the cell and go to the next cell 
+            // and check if it can be allocated with another random and valid number
+            do
+            {
+                // If there is not numbers left in the list to try next, 
+                // return to the previous cell and allocate it with a different number
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    return false;
+                }
+
+                // Take a random number from the numbers left in the list
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                // Remove the allocated value from the list
+                numsLeft.Remove(value);
+            }
+            while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+            
+            return true;
+        }
+
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                // Check all the cells in vertical direction
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                // Check all the cells in horizontal direction
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+            
+            // Check all the cells in the specific block
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+        
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<SudokuCell>();
+
+            // Find all the wrong inputs
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+
+            // Check if the inputs are wrong or the player wins 
+            if (wrongCells.Any())
+            {
+                // Highlight the wrong inputs 
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Wrong inputs");
+            }
+            else
+            {
+                MessageBox.Show("You Wins");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                // Clear the cell only if it is not locked
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+        }
+    }
+}
